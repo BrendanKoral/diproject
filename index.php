b3964bb9e16d46c08a19750bd058f8d2f5c92319
Brendan Koral <brendanhkoral@gmail.com>
2016-10-30 13:10:14 -0500

    Hello world, basic routing

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$page = $_GET['page'];
+
+function getData($page) {
+    $file = file_get_contents("data.json");
+    $json = json_decode($file, true);
+    return $json[$page];
+}
+
+function displayView($page) {
+    // echo '<h1>'.$page['header'].'</h1>';
+    // echo $page['body'];
+    
+    echo json_encode($page);
+}
+
+$json = getData($page);    
+displayView($json);
