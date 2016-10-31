f8114670d306cc5a7805e03205ed52ee4ff0836d
Brendan Koral <brendanhkoral@gmail.com>
2016-10-30 23:50:30 -0500

    I forget what changes I made but here they are

--- index.php
+++ index.php
@@ -3,20 +3,18 @@
 $page = $_GET['page'];
 
 function getData($page) {
-
-    var_dump($page);
-
     $file = file_get_contents("data.json");
     $json = json_decode($file, true);
-    return $page;
+    return $json[$page];
 }
 
 function displayView($page) {
-    echo '<h1>'.$page['header'].'</h1>';
-    echo $page['body'];
-    
-    echo json_encode($page);
+//     echo '<h1>'.$page['header'].'</h1>';
+//     echo $page['body'];
+
+    echo json_encode($page['header']);
+    echo json_encode($page['body']);
 }
 
-$json = getData($page);    
+$json = getData($page);
 displayView($json);
